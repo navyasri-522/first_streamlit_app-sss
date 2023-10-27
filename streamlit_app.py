31045355127f277fa141c2bb9b6be824c8d81651
navyasri-522 <110251316+navyasri-522@users.noreply.github.com>
2023-10-27 16:14:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,17 +12,15 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 streamlit.dataframe(fruityvice_normalized)
 
-
+#new section
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 
